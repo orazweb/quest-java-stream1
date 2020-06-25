b57d184c3d573c7add98982e42e089fb7f24b5b6
orazweb <jer.paoli@gmail.com>
2020-06-25 17:10:32 +0200

    stream done

--- Shield.java
+++ Shield.java
@@ -4,9 +4,12 @@ import java.util.stream.Collectors;
 
 public class Shield {
 
+    private static List<Hero> heroes;
+//    private List<Hero> ;
+
     public static void main(String[] args) {
 
-        List<Hero> heroes = new ArrayList<>();
+        heroes = new ArrayList<>();
         heroes.add(new Hero("Black Widow", 34, false));
         heroes.add(new Hero("Captain America", 100, false));
         heroes.add(new Hero("Vision", 3, true));
@@ -17,8 +20,25 @@ public class Shield {
         heroes.add(new Hero("Hulk", 49, true));
         heroes.add(new Hero("Doctor Strange", 42, false));
 
+  /* phase preparatoire de test pour recupèrer
+   * les valeurs de l'objet d'une classe dans une liste d'objet
+   * et vérifier le resultat escompté (liste des vieux de + de 60 balles)
+   */
+//        System.out.println(heroes.get(0).getAge());
+
+//        for (Hero hero: heroes) {
+//            System.out.println(hero.getAge());
+//        }
+
+//        for (Hero hero: heroes) {
+//            System.out.print(hero.getAge() > 60 ? hero.getName() + " " + hero.getAge() + " ans \n" : "");
+//        }
+
         List<Hero> elders;
         // TODO 1 : filter heroes in order to found heroes older than 59
+        elders = heroes.stream()
+                .filter(hero -> hero.getAge() > 60)
+                .collect(Collectors.toList());
 
         System.out.println("\nElders:");
         for (Hero elder : elders) {
@@ -26,8 +46,10 @@ public class Shield {
         }
 
         List<Hero> intolerants;
-        // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
-
+       // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
+        intolerants = heroes.stream()
+                .filter(hero -> hero.isGlutenIntolerant())
+                .collect(Collectors.toList());
         System.out.println("\nGluten intolerants:");
         for (Hero intolerant : intolerants) {
             System.out.println(intolerant.getName());
